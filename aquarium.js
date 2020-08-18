ccaeca0a68ccc692fb5a0142e62696167e496e5d
LiamN69 <69023006+LiamN69@users.noreply.github.com>
2020-08-18 12:37:54 +1000

    Update aquarium.js

--- aquarium.js
+++ aquarium.js
@@ -1,22 +1,24 @@
 window.onload=function() {
     lengthObj = document.getElementById('txtLength');
     widthObj = document.getElementById('txtwidth');
-    heightObj = document.getElementById('tdheight');
+    heightObj = document.getElementById('txtheight');
+	costObj = document.getElementById('tdcost');
     document.getElementById('btnReset').onclick = resetInputs;
-    document.getElementById('btnCalc').onclick = calcheight;
+    document.getElementById('btnCalc').onclick = calccost;
 }
 function resetInputs() {
     lengthObj.value = '';
     widthObj.value = '';
-    heightObj.innerHTML = '';
+	heightObj.value = '';
+    costObj.innerHTML = '';
 }
-function calcheight() {
+function calccost() {
     var length = new Number(lengthObj.value);
     var width = new Number(widthObj.value);
-    heightObj.innerHTML = '';
-    if(isNaN(length) || isNaN(width)) {
+	var height = new Number(heightObj.value);
+    costObj.innerHTML = '';
+    if(isNaN(length) || isNaN(width) || isNaN(height)) {
         alert('Invalid length or width');
         return;
     }
-    heightObj.innerHTML = length*Math.pow(width,2)/800;
-    }
+    costObj.innerHTML = (0.06* (2*(height*length) + 2*(height*width) + (width*length)) + (0.10 * 4 * height + 0.10 * 2* length + 0.10 * 2* width) + (2*(height * length) + 2*(height * width) + (width * length) / 6000 * 60)) *1.1   }
