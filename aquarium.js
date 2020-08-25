a3ec6d71901ea5e1dd517a84f4422d011f229472
LiamN69 <69023006+LiamN69@users.noreply.github.com>
2020-08-25 11:08:31 +1000

    Update aquarium.js

--- aquarium.js
+++ aquarium.js
@@ -18,7 +18,14 @@ function calccost() {
 	var height = new Number(heightObj.value);
     costObj.innerHTML = '';
     if(isNaN(length) || isNaN(width) || isNaN(height)) {
-        alert('Invalid length or width');
+        alert('Invalid length, width or height');
         return;
     }
-    costObj.innerHTML = (0.06* (2*(height*length) + 2*(height*width) + (width*length)) + (0.10 * 4 * height + 0.10 * 2* length + 0.10 * 2* width) + (2*(height * length) + 2*(height * width) + (width * length) / 6000 * 60)) *1.1   }
+if (length < 25) {
+	costObj.innerHTML = (0.06* (2*(height*length) + 2*(height*width) + (width*length)) + (0.10 * 4 * height + 0.10 * 2* length + 0.10 * 2* width) + (2*(height * length) + 
+	2*(height * width) + (width * length) / 6000 * 60)) *1.1   
+} else {
+	costObj.innerHTML = (0.1* (2*(height*length) + 2*(height*width) + (width*length)) + (0.10 * 4 * height + 0.10 * 2* length + 0.10 * 2* width) + (2*(height * length) + 
+	2*(height * width) + (width * length) / 6000 * 60)) *1.1   
+}
+}
